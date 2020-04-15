406cb6d07c744000a829e82987d98a239abbeee9
Charlie <cpg55850@gmail.com>
2020-04-15 04:31:37 -0500

    First commit

---
+++ Assets/ThirdPersonCharacterController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public float jumpForce = 7f;
+    private Rigidbody rb;
+    private CapsuleCollider col;
+    public LayerMask ground;
+    private bool doubleJump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement() {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            if(IsGround()) {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                doubleJump = false;
+			}
+
+            if(!IsGround() && !doubleJump) {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                doubleJump = true;     
+			}
+		}
+	}
+
+    private bool IsGround() {
+        bool isGround = Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius, ground);
+        return isGround;
+	}
+}
