11f04808da48bdbc209f1bd20e837af35e711619
Charlie <cpg55850@gmail.com>
2020-04-17 03:49:58 -0500

    Punchable

--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -22,12 +22,17 @@ public class ThirdPersonCharacterController : MonoBehaviour
     public Sprite fullHeart;
     public Sprite emptyHeart;
 
+    public float damage = 10f;
+    public float range;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         col = GetComponent<CapsuleCollider>();
         anim = GetComponent<Animator>();
+        range = 3f;
     }
 
     // Update is called once per frame
@@ -46,6 +51,12 @@ public class ThirdPersonCharacterController : MonoBehaviour
         if(Input.GetMouseButtonDown(0)) {
             Punch();
 		}
+
+        // MakeCube
+        if(Input.GetMouseButtonDown(1)) {
+            MakeCube();  
+		}
+        Debug.DrawRay(this.transform.position + new Vector3(0, 2, 0), this.transform.forward * range, Color.red, 0f, false);
 
         // Health
 
@@ -71,16 +82,28 @@ public class ThirdPersonCharacterController : MonoBehaviour
     }
 
     void Punch() {
-                    anim.Play("Punch",-1,0f);
+        anim.Play("Punch",-1,0f);
+        RaycastHit hit;
+        bool didHit = Physics.Raycast(this.transform.position + new Vector3(0, 2, 0), transform.forward, out hit, range);
 
-            GameObject player = this.gameObject;
-            Vector3 playerPos = player.transform.position;
-            Vector3 playerDirection = player.transform.forward;
-            Quaternion playerRotation = player.transform.rotation;
-            float spawnDistance = 10f;
+        if(didHit) {
+            
+              Debug.Log(hit.transform.name);
+              hit.rigidbody.AddForce(transform.forward * 400f);
+		}
+	}
 
-            Vector3 spawnPos = playerPos + playerDirection*spawnDistance;
-            Instantiate(upCube, spawnPos, playerRotation);
+    void MakeCube() {
+        
+
+        GameObject player = this.gameObject;
+        Vector3 playerPos = player.transform.position;
+        Vector3 playerDirection = player.transform.forward;
+        Quaternion playerRotation = player.transform.rotation;
+        float spawnDistance = 10f;
+
+        Vector3 spawnPos = playerPos + playerDirection*spawnDistance;
+        Instantiate(upCube, spawnPos, playerRotation);
 	}
 
     void PlayerMovement() {
