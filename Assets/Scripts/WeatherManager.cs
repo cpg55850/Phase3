de7a416a20ec2d54745e9089b53238b450723b44
Charlie <cpg55850@gmail.com>
2020-04-17 12:10:01 -0500

    improved animations

---
+++ Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour
+{
+    public Light sunLight;
+
+    public void Update() {
+        sunLight.transform.Rotate(0.01f,0,0);
+	}
+    
+}
