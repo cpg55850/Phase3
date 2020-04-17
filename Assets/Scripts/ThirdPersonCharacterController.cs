6a219268892a7d38bfea254d594b51fa20b9b80e
Charlie <cpg55850@gmail.com>
2020-04-16 23:48:58 -0500

    update

--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThirdPersonCharacterController : MonoBehaviour
 {
@@ -12,6 +13,14 @@ public class ThirdPersonCharacterController : MonoBehaviour
     public LayerMask ground;
     private bool doubleJump = false;
     private Animator anim;
+    public GameObject upCube;
+
+    public int health;
+    public int maxHealth;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +33,7 @@ public class ThirdPersonCharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Walking animation
         PlayerMovement();
 
         float inputH = Input.GetAxis("Horizontal");
@@ -32,7 +42,46 @@ public class ThirdPersonCharacterController : MonoBehaviour
         anim.SetFloat("inputH", inputH);
         anim.SetFloat("inputV", inputV);
 
+        // Punch
+        if(Input.GetMouseButtonDown(0)) {
+            Punch();
+		}
+
+        // Health
+
+        if(health > maxHealth) {
+            health = maxHealth;  
+		}
+
+        for(int i = 0; i < hearts.Length; i++) {
+            if(i < health) {
+                hearts[i].sprite = fullHeart;     
+			} else {
+                hearts[i].sprite = emptyHeart;     
+			}
+
+            if(i < maxHealth) {
+                hearts[i].enabled = true;
+		    } else {
+                hearts[i].enabled = false;
+		    }
+	    }
+
+
     }
+
+    void Punch() {
+                    anim.Play("Punch",-1,0f);
+
+            GameObject player = this.gameObject;
+            Vector3 playerPos = player.transform.position;
+            Vector3 playerDirection = player.transform.forward;
+            Quaternion playerRotation = player.transform.rotation;
+            float spawnDistance = 10f;
+
+            Vector3 spawnPos = playerPos + playerDirection*spawnDistance;
+            Instantiate(upCube, spawnPos, playerRotation);
+	}
 
     void PlayerMovement() {
         float hor = Input.GetAxis("Horizontal");
